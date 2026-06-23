a086fdccec4286bf8910fba31137a1ec21a943b5
Tobias Obregon <tobiobregon87@gmail.com>
2026-06-23 17:52:06 -0300

    fix: usar PORT dinamico para Railway

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors({ origin: '*', methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', credentials: true }));
 app.use(express.json());
